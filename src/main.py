07bd742d1a9afe569d07eee26352237576370c40
istiakahmedbappi <istiakbappi@gmail.com>
2026-08-28 21:13:33 +0600

    Add multiply and divive function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply, divide
 print("Istiak Ahmed Bappi")
 print(date.today())
 print("5 + 3 =", add(5, 3))
 print("5 - 3 =", subtract(5, 3))
+print("5 * 3 =", multiply(5, 3))
+print("5 / 3 =", divide(5, 3))
